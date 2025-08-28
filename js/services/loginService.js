4cd8ede3b0c9c4d090fa9a67143b18a214b5dadd
GerardoAJF <74077723+GerardoAJF@users.noreply.github.com>
2025-08-27 20:37:14 -0600

    feat: login básico funcional

---
+++ js/services/loginService.js
@@ -0,0 +1,20 @@
+import { APIURL } from "../../utils/api_url";
+
+export async function validateLogin(email, password) {
+  try {
+    const response = await fetch(
+      `${APIURL}apiUser/loginUser?email=${email}&password=${password}`,
+      {
+        method: "POST",
+      }
+    );
+    if (!response.ok) {
+        console.error("Error validating user");
+    };
+
+    return response.json();
+  } catch (err) {
+    console.error(`Error validating user: ${err}`);
+    throw err;
+  }
+}
