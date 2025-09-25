23576c2b022bbf285f8bd4f07a4aea0d55304cee
AbelVJ <oscar.joyar@gmail.com>
2025-09-24 22:56:29 -0600

    Validar si esta en el login

--- js/controllers/sessionController.js
+++ js/controllers/sessionController.js
@@ -2,31 +2,30 @@ import * as AuthService from '../services/authService'
 
 ///Objeto con la informacion del usuario
 export const auth = {
-  ok: false, ///Sesion activa
-  user: null ///Datos del usuario
+    ok: false, ///Sesion activa
+    user: null ///Datos del usuario
 }
 
 /// Renderiza la caja de usuario y ajusta el menú según el estado de autenticación
-export async function renderKarbonZeroData() {
-  ;
+export async function renderKarbonZeroData() {;
   try {
-    const isLogin = /\/login(\.html)?$/.test(location.pathname);
+        const isLogin = /\/login(\.html)?$/.test(location.pathname);
     if (isLogin) {
       auth.ok = false;
       auth.user = null;
       return; // no llamamos a /me aquí
     }
     const info = await AuthService.getLoggedUser();
-    auth.ok = !!info?.authenticated;
+    auth.ok = !!info?.authenticated; 
     auth.user = info?.user ?? null;
     console.log(auth);
     if (auth.ok) {
-      document.getElementById("logout")?.addEventListener("click", async () => {
-        await AuthService.logout();
-        auth.ok = false;
-        auth.user = null;
-        window.location.replace("login.html");
-      });
+        document.getElementById("logout")?.addEventListener("click", async () => {
+          await AuthService.logout();            
+          auth.ok = false;
+          auth.user = null;
+          window.location.replace("login.html");
+        });
     } else {
       auth.ok = false;
       auth.user = null;
@@ -40,34 +39,34 @@ export async function renderKarbonZeroData() {
 
 ///Esta funcion nos sirve para poder saber el usuario esta auth y si no mandarlo al login
 export async function isAuth(redirect = true) {
-  console.log('I have been activated')
-  try {
-    const info = await AuthService.getLoggedUser()
-    auth.ok = !!info?.authenticated
-    auth.user = info.user ?? null;
-  } catch {
-    auth.ok = false;
-    auth.user = null;
-  }
-  if (!auth.ok && redirect) {
-    window.location.replace('login.html')
-  }
-  return auth.ok ///retornamos el valor de auth.ok(true o false), aunque siempre si es false se manda al login
+    console.log('I have been activated')
+    try {
+        const info = await AuthService.getLoggedUser()
+        auth.ok = !!info?.authenticated
+        auth.user = info.user ?? null;
+    } catch {
+        auth.ok = false;
+        auth.user = null;
+    }
+    if (!auth.ok && redirect) {
+        window.location.replace('login.html')
+    }
+    return auth.ok ///retornamos el valor de auth.ok(true o false), aunque siempre si es false se manda al login
 }
 
 export function getUserStatus() {
-  return auth.user?.status;
+    return auth.user?.status;
 }
 export function getUserAuthorities(authority) {
-  ///Devuelve las authorities del usuario
-  return Array.isArray(auth.user?.authorities) ? auth.user.authorities.includes(authority) : false
+    ///Devuelve las authorities del usuario
+    return Array.isArray(auth.user?.authorities) ? auth.user.authorities.includes(authority) : false
 }
 
 
 export const role = {
-  hasStatus0: () => getUserStatus() === 0 || getUserAuthorities("ROLE_0"),
-  hasStatus1: () => getUserStatus() === 1 || getUserAuthorities("ROLE_1"),
-  hasStatus2: () => getUserStatus() === 2 || getUserAuthorities("ROLE_2")
+    hasStatus0: () => getUserStatus() === 0 || getUserAuthorities("ROLE_0"),
+    hasStatus1: () => getUserStatus() === 1 || getUserAuthorities("ROLE_1"),
+    hasStatus2: () => getUserStatus() === 2 || getUserAuthorities("ROLE_2")
 }
 
 // ///El evento pageshow se dispara siempre que se cargue la pagina, SIEMPRE, SIEMPRE, SIEMPRE, SIEMPRE, SIEMPRE, SIEMPRE, SIEMPRE
