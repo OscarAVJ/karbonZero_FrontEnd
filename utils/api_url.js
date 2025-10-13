1662f0987b6783e46825ac7a97d388533bf7b953
AbelVJ <oscar.joyar@gmail.com>
2025-10-13 01:39:43 -0600

    Ya api todo

--- utils/api_url.js
+++ utils/api_url.js
@@ -1,2 +1,2 @@
-export const APIURL = "https://karbonzero-management-14fb5e6b8dab.herokuapp.com/"
-export const APIGLOBAL = "http://localhost:8081/"
+export const APIURL = "https://karbonzero-institutional-26727f445378.herokuapp.com/"
+export const APIGLOBAL = "https://karbonzero-management-14fb5e6b8dab.herokuapp.com/"
